c493b57aaf5b4efc68efcfee4144cb10c49df3a6
Ruby Tubes De Beer <66409964+RubenDebeer@users.noreply.github.com>
2025-09-27 09:34:16 +0200

    Update write_service.go

--- internal/core/write_service.go
+++ internal/core/write_service.go
@@ -1,1 +1,15 @@
 package core
+
+import (
+	"github.com/RubenDeBeer/Go_Do_Something/internal/port"
+)
+
+type WriteService struct{
+repo port.repository
+}
+
+// code looks kak , kinda difficult to do it via the phone.
+
+func NewWriteService(r ports.repository) *WireService{
+
+}
